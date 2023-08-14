f3670f41aa23a28c5f3612f2c558a4a0602dc737
Bailey Pointer <thescrantonstrangler@outlook.com>
2023-08-14 06:29:39 -0500

    Clean deployment

---
+++ src/Home/Home.js
@@ -0,0 +1,9 @@
+import CurrentWeather from "./CurrentWeather/CurrentWeather";
+function Home() {
+    return (
+        <div>
+        <CurrentWeather />
+        </div>
+    )
+}
+export default Home;
